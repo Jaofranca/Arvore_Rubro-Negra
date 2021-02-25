2dd316d24d62c2aa7c6280658efac04962fdbe37
joao <joaovfranca@hotmail.com>
2021-02-24 23:11:20 -0300

    adicionado função de inserir na Árvore

--- Arvore.java
+++ Arvore.java
@@ -1,5 +1,6 @@
 package ifce.estruturadados.Arvorerubronegra;
 
+
 public class Arvore {
 	Node raiz;
 	Node nil;
@@ -9,13 +10,42 @@ public class Arvore {
 		this.raiz = null;
 	}
 	
-	public void inserirNo(Integer key) {
-		if(raiz == null) {
-			raiz = new Node(false,key,null,null,null);
-			return;
+	
+	public void insereNo(Integer valor,Node noBase) {
+			if(raiz == null) {
+				raiz = new Node(true,valor,null,null,null);
+				
+			}
+			if(valor > noBase.getKey()) {
+				if(noBase.getRight() == null) {
+					Node novoNo = new Node(true,valor,null,null,noBase);
+					noBase.setRight(novoNo);
+				}
+				else {
+					insereNo(valor,noBase.getRight());
+				}
+				
+			}else if(valor < noBase.getKey()) {
+					if(noBase.getLeft() == null) {
+						Node novoNo = new Node(true,valor,null,null,noBase);
+						noBase.setLeft(novoNo);
+					}
+					else {
+						insereNo(valor,noBase.getLeft());
+					}
+				
+				
+			}else if(valor == noBase.getKey()) {
+				System.out.println("Valor " + valor + "Ja foi inserido na arvore");
+			}
 		}
 		
-	}
+	// public void re_estruturarArvore(Node x) {
+	//	Node pai = x.getPai();
+	//	if(pai == pai.getPai().getLeft()) {
+	//		Node y = pai.getPai().getRight();
+	//	}
+	//}
 	public void RotacionarEsquerda(Node x) {
 		Node y = x.getRight();
 		Node pai = x.getPai();
